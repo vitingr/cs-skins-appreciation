aa034fbc23819e39f5294dde3f52bf1e09b968e2
vitorgsilvax <vgsilva@tractian.com>
2025-06-12 11:41:44 -0300

    remove axios

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,26 +1,12 @@
-import type { AxiosInstance } from "axios";
-import axios from "axios";
 import { apiBaseUrl } from "../../constants/environments/apiBaseUrl";
 import { apiKey } from "../../constants/environments/apiKey";
 import type { GetUserInventoryItemsData } from "./types";
 
 export class API {
-  private instance: AxiosInstance;
-
-  constructor() {
-    this.instance = axios.create({
-      baseURL: apiBaseUrl,
-      headers: {
-        "x-api-key": apiKey,
-        "Content-Type": "application/json",
-      },
-    });
-  }
-
   getUserInventoryItems = async ({ userId }: GetUserInventoryItemsData) => {
     try {
       const proxyUrl = `https://corsproxy.io/?url=${encodeURIComponent(
-        `https://www.steamwebapi.com/steam/api/inventory?key=${apiKey}&steam_id=${userId}`,
+        `${apiBaseUrl}/inventory?key=${apiKey}&steam_id=${userId}`,
       )}`;
 
       const response = await fetch(proxyUrl);
